9b022ccf3b7276c71a75eeeed85d88f804d0e4db
ErrPardo <114981213+ErrPardo@users.noreply.github.com>
2023-12-26 13:23:19 +0000

    Create simul_ext.c

---
+++ simul_ext.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    while(1){
+        if(){
+
+        }
+        else if(){
+
+        }
+        else if(){
+
+        }
+        else if(){
+
+        }
+        else if(){
+
+        }
+        else if(){
+
+        }
+        else if(){
+
+        }
+        else{
+
+        } 
+    } 
+    return 0;
+}
